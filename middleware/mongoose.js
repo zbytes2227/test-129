96fad63076a2f12aedb615e9e121af532c016138
Ujjwal Kushwaha <111581344+zbytes2227@users.noreply.github.com>
2025-01-19 10:46:02 +0530

    Update mongoose.js

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,7 @@ const connectDb = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
-  await mongoose.connect("mongodb+srv://zbytes1:yvnwXhOgOl6p44FS@cluster0.45jhcsq.mongodb.net/aim?retryWrites=true&w=majority");
+  await mongoose.connect("mongodb+srv://zbytes1:yvnwXhOgOl6p44FS@cluster0.45jhcsq.mongodb.net/esptester?retryWrites=true&w=majority");
   return handler(req, res);
 };
 export default connectDb;
